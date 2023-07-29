af90bfeab35452ad038cb3d873c8da8ba3eb36a8
hayeooo <hayeon0709@daum.net>
2023-07-29 18:43:34 +0900

    코드 추가

--- src/Collection/Main.java
+++ src/Collection/Main.java
@@ -2,15 +2,31 @@ package Collection;
 
 import java.util.*;
 import java.io.*;
+/*
+ * 두 객체의 내용을 비교할 수 있도록 equals 메소드 재정의
+ * */
+class User{
+	public String name;
+	public User(String name) {
+		this.name=name;
+	}
+	public boolean equals(Object obj) {
+		if (obj!=null && obj instanceof User) {
+			User usr=(User)obj;
+			return this.name.equals(usr.name);
+		}
+		return false;
+	}
+	public int hashCode() {
+		return this.name.hashCode();
+	}
+}
 public class Main {
 	public static void main(String[] args) {
-		List<Integer> arrList=new ArrayList<>();
-		arrList.add(0);
-		arrList.add(2);
-		arrList.add(1);
-		arrList.add(2);
+		Set<User> set=new HashSet<>();
+		set.add(new User("h"));
+		set.add(new User("h"));
 		
-		arrList.remove(1);
-		System.out.println(arrList);
+		System.out.println(set.size());
 	}
 }
