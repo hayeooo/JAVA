69f529612ca30aacfa7feb1fe36e0145031b24f7
hayeooo <hayeon0709@daum.net>
2023-07-30 11:44:14 +0900

    equals & hashCode

---
+++ src/Basic/ArrayInvestigate.java
@@ -0,0 +1,27 @@
+package Basic;
+
+public class ArrayInvestigate {
+	public static void main(String[] args) {
+		int sum=0;
+		char[][] grid=new char[4][4];
+		int[][] deltas= {{-1,0},{1,0},{0,-1},{0,1}};
+		/*
+		for (int r=0;r<4;r++) {
+			for(int c=0;c<4;c++) {
+				if (grid[r][c]=='X') {
+					for (int d=0;d<4;d++) {
+						int nr=r+grid[d][0];
+						int nc=c+grid[d][1];
+						if (nr>=0 && nr<4 && nc>=0 && nc<4 && grid[nr][nc]!='X') {
+							sum+=grid[nr][nc]-'0';
+							grid[nr][nc]='0';
+						}
+					}
+				}
+			}
+		}
+		*/
+		Arrays.copyOf
+		
+	}
+}
--- src/Collection/SetEqualsHash.java
+++ src/Collection/SetEqualsHash.java
@@ -1,7 +1,11 @@
 package Collection;
 
+import java.util.HashSet;
+import java.util.Set;
+
 class SmartPhone{
 	String number;
+	String name="김";
 	public SmartPhone(String number) {
 		this.number=number;
 	}
@@ -13,15 +17,24 @@ class SmartPhone{
 		boolean result=false;
 		if (obj!=null && obj instanceof SmartPhone) {
 			SmartPhone param=(SmartPhone)obj;
+			//System.out.println(this.number==param.number);
 			result=(this.number.equals(param.number));
 		}
 		return result;
 	}
+	// Set을 사용할 때는 hashCode 필수
+	// HashTable, HashSet, HashMap과 같은 자료구조는 자료를 저장하기 위한 위치를 선택하기 위해 hashCode 사용
 	@Override
 	public int hashCode() {
-		return number.hashCode();
+		// int 형태로 반환해주기만 하면 됨.
+		return (number+name).hashCode();
 	}
 }
 public class SetEqualsHash {
-
+	public static void main(String[] args) {
+		Set<SmartPhone> set=new HashSet<>();
+		set.add(new SmartPhone("010"));
+		set.add(new SmartPhone("010"));
+		System.out.println(set.size());
+	}
 }
