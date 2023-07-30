1796cad3b4e65211a61af1b4696b9a990ecfc4b7
hayeooo <hayeon0709@daum.net>
2023-07-30 17:40:16 +0900

    Comparator 구현

--- src/Collection/SortTest.java
+++ src/Collection/SortTest.java
@@ -2,15 +2,31 @@ package Collection;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
-class Person implements Comparable<Person>{
+class PersonSorting implements Comparator<Person>{
+
+	@Override
+	public int compare(Person o1, Person o2) {
+		// 이름 길이 내림차순 1차 정렬
+		if (o1.name.length()<o2.name.length()) return 1;
+		// 나이 오름차순 2차 정렬
+		if (o1.age>o2.age) return 1;
+		// 이름 사전 순 3차 정렬
+		return o1.name.compareTo(o2.name);
+	}
+	
+}
+class Person{
 	String name;
 	int age;
 	public Person(String name, int age) {
 		this.name=name;
 		this.age=age;
 	}
+	/*
+	 * Comparable implements
 	@Override
 	public int compareTo(Person o) {
 		// 이름 길이 내림차순 1차 정렬
@@ -20,6 +36,7 @@ class Person implements Comparable<Person>{
 		// 이름 사전 순 3차 정렬
 		return this.name.compareTo(o.name);
 	}
+	*/
 	public String toString() {
 		return "name: "+name+" age: "+age;
 	}
@@ -33,7 +50,7 @@ public class SortTest {
 		arrList.add(new Person("b",10));
 		arrList.add(new Person("a",10));
 		arrList.add(new Person("b",12));
-		Collections.sort(arrList);
+		Collections.sort(arrList, new PersonSorting());
 		System.out.println(arrList);
 		
 	}
